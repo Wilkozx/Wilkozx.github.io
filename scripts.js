e17d7678caad466ff538cc9dec951fd294d7cfc5
Wilkozx <wilkoisontheceiling@gmail.com>
2024-02-05 21:30:30 +0000

    ✨better site✨
    - redid site

---
+++ scripts.js
@@ -0,0 +1,8 @@
+homebutton.addEventListener('click', ev => {
+  window.location.href = 'index.html';
+});
+
+morebutton.addEventListener('click', ev => {
+    morebutton.classList.toggle('open');
+});
+
