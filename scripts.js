41ced5384fe0212001e10bc7892ff7b840ddb0d1
Wilkozx <wilkoisontheceiling@gmail.com>
2024-03-02 05:01:18 +0000

    🚧
    - just updating github so I can work on other device

--- scripts.js
+++
@@ -1,8 +0,0 @@
-homebutton.addEventListener('click', ev => {
-  window.location.href = 'index.html';
-});
-
-morebutton.addEventListener('click', ev => {
-    morebutton.classList.toggle('open');
-});
-
